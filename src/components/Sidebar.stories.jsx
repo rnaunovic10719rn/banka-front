0cf0b75d4034f32d1c28cb4c73e0310c19daa4b4
snerror <vladetaputnikovic@gmail.com>
2022-03-31 00:36:46 +0200

    added sidebar

---
+++ src/components/Sidebar.stories.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Sidebar from "./Sidebar";
+
+export default {
+    title: "Components/Sidebar",
+    component: Sidebar,
+};
+
+const Template = (args) => <Sidebar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+};
