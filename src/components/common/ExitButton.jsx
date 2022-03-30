cbea3b1193d532ab95aadaad9d42350c54a46932
snerror <vladetaputnikovic@gmail.com>
2022-03-31 00:16:52 +0200

    added alert component

---
+++ src/components/common/ExitButton.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+function ExitButton(props) {
+    const stlye = classNames(
+        " bg-transparent text-sm inline-flex items-center",
+        { "text-gray-500 hover:text-gray-900": !props.color },
+        props.color,
+    )
+
+    return (
+        <button type="button" className={stlye} onClick={props.onClick}>
+            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd">
+                </path>
+            </svg>
+        </button>
+    )
+}
+
+ExitButton.propTypes = {
+    color: PropTypes.string,
+    onClick: PropTypes.func.isRequired,
+}
+
+export default ExitButton
