1917f076d7705b95519f67f5983e153e496c1199
vmatovic <vmatovic3818rn@raf.rs>
2022-06-10 23:08:51 +0200

    Test primer tabela

--- src/pages/dashboard/capitalsecurity.jsx
+++ src/pages/dashboard/capitalsecurity.jsx
@@ -1,10 +1,32 @@
 import React from "react"
+import Block from "../../components/common/Block";
+import Table from "../../components/common/Table";
+
+const testExample = [
+    ["Apple Inc.", "15", "$163.64", "$2,454", "$2,400", "$54"],
+    ["Apple Inc.", "15", "$163.64", "$2,454", "$2,400", "$54"],
+    ["Apple Inc.", "15", "$163.64", "$2,454", "$2,400", "$54"],
+    ["Apple Inc.", "15", "$163.64", "$2,454", "$2,400", "$54"]
+];
 
 export default function CapitalSecurityPage() {
     const title = window.location.pathname.split("/")[2];
+
     return (
         <div>
-            {title}
+            <Block title={title}>
+            <Table
+                headings={[
+                    "Naziv",
+                    "Količina",
+                    "Trenutna cena",
+                    "Ukupno",
+                    "Plaćeno",
+                    "Razlika"
+                  ]}
+                rows={testExample}
+            />
+            </Block>
         </div>
     )
 }
