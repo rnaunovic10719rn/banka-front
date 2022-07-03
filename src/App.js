ec7b1a84627d529cd6591fe61180515b3fe941ad
snerror <vladetaputnikovic@gmail.com>
2022-07-03 12:04:23 +0200

    style correction

--- src/App.js
+++ src/App.js
@@ -6,23 +6,23 @@ import { addUserAction } from "./redux/actions";
 import nightwind from "nightwind/helper";
 
 function App(props) {
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	async function getUser() {
-		const response = await getUserApi();
-		dispatch(addUserAction(response));
-	}
+    async function getUser() {
+        const response = await getUserApi();
+        dispatch(addUserAction(response));
+    }
 
-	useEffect(() => {
-		getUser();
-	}, []);
+    useEffect(() => {
+        getUser();
+    }, []);
 
-	return (
-		<div data-testid="app" className="h-screen bg-slate-50 text-slate-700">
-			<script dangerouslySetInnerHTML={{ __html: nightwind.init() }} />
-			<Outlet />
-		</div>
-	);
+    return (
+        <div data-testid="app" className="h-full bg-slate-50 text-slate-700 pb-10">
+            <script dangerouslySetInnerHTML={{__html: nightwind.init()}}/>
+            <Outlet/>
+        </div>
+    );
 }
 
 export default App;
